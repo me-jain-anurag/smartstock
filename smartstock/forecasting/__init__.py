ec0d6e881cf4bfa7b13bb82905e8e1487f57383c
me-jain-anurag <132147016+me-jain-anurag@users.noreply.github.com>
2026-02-26 00:39:10 +0530

    fix: typo in module docstring ('seies' -> 'series')

---
+++ smartstock/forecasting/__init__.py
@@ -0,0 +1,8 @@
+"""
+Forecasting module for SmartStock.
+Contains various time series forecasting models.
+"""
+
+from smartstock.forecasting.prophet_forecaster import ProphetForecaster
+
+__all__ = ["ProphetForecaster"]
