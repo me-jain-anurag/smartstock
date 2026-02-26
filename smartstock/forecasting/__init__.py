34d8278532ebe072e23eeb09ecf669bc60d6f978
me-jain-anurag <132147016+me-jain-anurag@users.noreply.github.com>
2026-02-26 14:15:50 +0530

    chore: Update __init.py__ to export SARIMAForecaster from forecasting module

--- smartstock/forecasting/__init__.py
+++ smartstock/forecasting/__init__.py
@@ -1,8 +1,10 @@
 """
 Forecasting module for SmartStock.
-Contains various time series forecasting models.
+
+This module contains forecasting models for time series prediction.
 """
 
 from smartstock.forecasting.prophet_forecaster import ProphetForecaster
+from smartstock.forecasting.sarima_forecaster import SARIMAForecaster
 
-__all__ = ["ProphetForecaster"]
+__all__ = ["ProphetForecaster", "SARIMAForecaster"]
